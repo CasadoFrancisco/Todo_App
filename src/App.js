d197b793fcece5b77bdffcd5fe6561611635f2c5
CasadoFrancisco <casadofranciscoromero@gmail.com>
2023-02-07 11:58:37 -0300

    second change

--- src/App.js
+++ src/App.js
@@ -1,87 +1,80 @@
 import "./App.css";
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
 
-      const [todo, setTodo]= useState('');
+  const [todos, setTodos] = useState([]);
 
-      const [todos, setTodos]= useState([]);
+  const [editId, setEditId] = useState(0);
 
-      const[editId, setEditId]= useState(0)
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
 
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo("");
+    }
+  };
 
-      const handleSubmit = (e)=>{
-        e.preventDefault();
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
 
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
 
-        if(editId){
-          const editTodo = todos.find((i)=>i.id === editId);  
-          const updatedTodos = todos.map((t)=>
-          t.id === editTodo.id
-          ? (t = { id: t.id, todo})
-          :{id:t.id,todo:t.todo}
-          );
-          setTodos(updatedTodos);
-          setEditId(0);
-          setTodo("")
-          return
-        }
-
-        if(todo!==''){
-          setTodos([{id:`${todo}-${Date.now()}`,todo}, ...todos]);
-          setTodo("");
-        }
-      }
-
-      const handleDelete = (id)=>{
-        const delTodo=todos.filter((to)=>to.id !==id);
-        setTodos([...delTodo])
-      }
-
-      const handleEdit=(id)=>{
-
-        const editTodo = todos.find((i)=>i.id === id);
-
-        setTodo(editTodo.todo)
-        setEditId(id)
-      };
-
-
-
-
-
-
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
 
   return (
     <div className="App" transition-style="in:circle:center">
       <div className="container">
         <h1>ToDo App</h1>
         <form className="todoForm" onSubmit={handleSubmit}>
-          <input type="text" value={todo} onChange={(e)=> setTodo(e.target.value)}/>
-          <button type="submit">{editId ? "Edit":"Go"}</button>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button className="iconSubmit" type="submit">{editId ? "Edit" : "Submit"}</button>
         </form>
 
         <ul className="allTodos">
-
-          {
-            todos.map((t)=> (
-              <li className="singleTodo">
-              
-              
-            <span className="todoText" key={t.id}>
-            {t.todo}
-            </span>
-            <button class="material-symbols-outlined" onClick={()=>handleEdit(t.id)}>edit</button>
-            <button class="material-symbols-outlined" onClick={()=>handleDelete(t.id)} >delete</button> 
-          </li>
-            ))
-          }
-
-
-
-          
-          
+          {todos.map((t) => (
+            <li className="singleTodo">
+              <span className="todoText" key={t.id}>
+                {t.todo}
+              </span>
+              <button
+                className="icon material-symbols-outlined"
+                onClick={() => handleEdit(t.id)}
+              >
+                edit
+              </button>
+              <button
+                className="icon material-symbols-outlined"
+                onClick={() => handleDelete(t.id)}
+              >
+                delete
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
